fa65391b81712179ac0257f631496233a7d3b962
Harshvardhan <harshvardhanrokade845@gmail.com>
2025-07-03 07:53:19 +0530

    make the cart quantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -58,6 +58,9 @@ products.forEach((product)=>{
 document.querySelector('.js-products-grid').
   innerHTML = productsHTML;
 
+
+
+
 document.querySelectorAll('.js-add-to-cart').
   forEach((button) => {
     button.addEventListener('click' , () => {
@@ -80,6 +83,15 @@ document.querySelectorAll('.js-add-to-cart').
           quantity : 1
         });
       }
-      console.log(cart);
+
+
+      let cartQuantity = 0;
+
+      cart.forEach((item) => {
+        cartQuantity += item.quantity;
+      });
+
+      document.querySelector('.js-cart-quantity').
+        innerHTML = cartQuantity;
     });
   });
